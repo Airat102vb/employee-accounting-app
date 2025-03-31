dd3a6a591619c8c3bc682e2766433378a0e7d957
Airat <airat102vb@gmail.com>
2025-03-31 08:53:33 +0300

    added bootstrap to user-service and configured getting local properties

---
+++ user-service/src/main/java/pet/project/ConfigChecker.java
@@ -0,0 +1,35 @@
+package pet.project;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConfigChecker implements CommandLineRunner {
+
+  @Value("${spring.datasource.url}")
+  private String dbUrl;
+
+  @Value("${spring.datasource.username}")
+  private String dbUser;
+
+  private final Environment env;
+
+  public ConfigChecker(Environment env) {
+    this.env = env;
+  }
+
+  @Override
+  public void run(String... args) {
+    System.out.println("=== Active Profiles ===");
+    for (String profile : env.getActiveProfiles()) {
+      System.out.println(profile);
+    }
+
+    System.out.println("=== Config from Config Server ===");
+    System.out.println("DB URL: " + dbUrl);
+    System.out.println("DB User: " + dbUser);
+    System.out.println("================================");
+  }
+}
