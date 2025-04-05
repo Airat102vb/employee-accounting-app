7dbecd4d2c631c6c04a4150bbf77bde71bb9bc8a
Airat <airat102vb@gmail.com>
2025-04-05 09:52:23 +0300

    configured with cloud config

--- company-service/src/main/java/pet/project/CompanyServiceApplication.java
+++ company-service/src/main/java/pet/project/CompanyServiceApplication.java
@@ -3,11 +3,17 @@ package pet.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class CompanyServiceApplication {
   public static void main(String[] args) {
+    ApplicationContext context = new AnnotationConfigApplicationContext();
+    Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
     SpringApplication.run(CompanyServiceApplication.class, args);
   }
 }
