2b36dd4140d554984d55e50129b4c968cfdd3028
Airat <airat102vb@gmail.com>
2025-04-09 00:27:10 +0300

    config docker compose

--- api-gateway/src/main/java/pet/project/GatewayApplication.java
+++ api-gateway/src/main/java/pet/project/GatewayApplication.java
@@ -3,9 +3,12 @@ package pet.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
+import pet.project.config.Config;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Import(Config.class)
 public class GatewayApplication {
   public static void main(String[] args) {
     SpringApplication.run(GatewayApplication.class, args);
---
+++ api-gateway/src/main/java/pet/project/config/Config.java
@@ -0,0 +1,8 @@
+package pet.project.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pet.project")
+public class Config {}
