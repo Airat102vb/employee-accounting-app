15c30528fcb7583fcae5601da786474cef050250
Airat <airat102vb@gmail.com>
2025-05-01 19:08:54 +0300

    DataSourceConfigurationPropertie and Config are deleted as unnecessary. And spring-boot-starter-jdbc is deleted because hibernate in use

--- company-service/src/main/java/pet/project/CompanyServiceApplication.java
+++ company-service/src/main/java/pet/project/CompanyServiceApplication.java
@@ -4,21 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Import;
-import pet.project.config.Config;
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@Import(Config.class)
 @EnableFeignClients
 public class CompanyServiceApplication {
   public static void main(String[] args) {
-    ApplicationContext context = new AnnotationConfigApplicationContext();
-    Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
-
     SpringApplication.run(CompanyServiceApplication.class, args);
   }
 }
--- user-service/src/main/java/pet/project/UserServiceApplication.java
+++ user-service/src/main/java/pet/project/UserServiceApplication.java
@@ -1,24 +1,15 @@
 package pet.project;
 
-import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Import;
-import pet.project.config.Config;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@Import(Config.class)
 public class UserServiceApplication {
   public static void main(String[] args) {
-    ApplicationContext context = new AnnotationConfigApplicationContext();
-    Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
-
     SpringApplication.run(UserServiceApplication.class, args);
   }
 }
